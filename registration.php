90093f5f6f1315ee72f2228f3a87ba6d554f30e0
bernard maina <njugunabernard972@gmail.com>
2024-06-25 10:20:45 +0300

    imported the nav.php file into the regisration page

--- registration.php
+++ registration.php
@@ -12,13 +12,7 @@
 </head>
 <body>
     <!--this is the main body-->
-    <div class="nav">
-        <a href="index.php">Home</a> 
-     <a href="about.php">About</a> 
-      <a href="registration.php">Registration</a> 
-     <a href="contact.php">Contact</a>
-     <a href="Policy.php">Policy</a>
-    </div>
+  <?php include_once("templates/nav.php");?>
     <div class="banner">
         <h1>BENA TECH CO.</h1> 
         </div>
