f7462837893ab3758a5a602097b5cdd39e22b6e7
bernard maina <njugunabernard972@gmail.com>
2024-06-25 10:16:43 +0300

    added nav.php file in templates

---
+++ templates/nav.php
@@ -0,0 +1,7 @@
+<div class="nav">
+        <a href="index.php">Home</a> 
+     <a href="about.php">About</a> 
+      <a href="registration.php">Registration</a> 
+     <a href="contact.php">Contact</a>
+     <a href="Policy.php">Policy</a>
+    </div>
